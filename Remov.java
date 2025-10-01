94cec91f7072abcfe6931e3d3099e04ed45490df
Jahnavim12 <168292840+Jahnavim12@users.noreply.github.com>
2025-10-01 19:51:54 +0530

    Remove Element

---
+++ Remov.java
@@ -0,0 +1,9 @@
+class Remov {
+    public int removeElement(int[] a, int v) {
+        int k = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != v) a[k++] = a[i];
+        }
+        return k;
+    }
+}
