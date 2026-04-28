0a19bf706b9a46ada1cb0879c5328bdffa1c78ef
Jahnavim12 <168292840+Jahnavim12@users.noreply.github.com>
2026-04-28 11:46:47 +0530

    Add solution for Sort Colors

--- LeetCode/Sort-Colors/Solution.java
+++ LeetCode/Sort-Colors/Solution.java
@@ -1,8 +1,14 @@
-[0]
-[2,0]
-[0,0,0,1,1,1,2,2,2]
-[2,2,2,0,0,0,1,1,1]
-[2,2,1,1,0,0,0]
-[2,1,2,2,2,2,0,0,0]
-[2,2,2,2,2,1,0,0,1,1,0]
-[2,2,2,1,1,0,0,1,0,1,0,2,1,0,2,1,0,2,1,1]
+1class Solution {
+2    public static int[] sortColors(int[] nums) {
+3        for(int i=0;i<nums.length;i++){
+4            for(int j=0;j<nums.length-1-i;j++){
+5                if(nums[j]>nums[j+1]){
+6                   int temp=nums[j];
+7                    nums[j]=nums[j+1];
+8                    nums[j+1]=temp;
+9                }
+10            }
+11        }
+12        return nums;
+13    }
+14}
