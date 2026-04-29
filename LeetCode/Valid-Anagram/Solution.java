ab3d50ea95d5cff6b5cfd339fa9a299c2f4a4e21
Jahnavim12 <168292840+Jahnavim12@users.noreply.github.com>
2026-04-29 11:56:26 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.java
+++ LeetCode/Valid-Anagram/Solution.java
@@ -1,10 +1,16 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] c = new int[26];
-        for (char x : s.toCharArray()) c[x - 'a']++;
-        for (char x : t.toCharArray()) if (--c[x - 'a'] < 0)
-             return false;
-        return true;
-    }
-}
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if (s.length() != t.length()) return false;
+4        char[] ch=s.toCharArray();
+5        char[] ch2=t.toCharArray();
+6        Arrays.sort(ch);
+7        Arrays.sort(ch2);
+8        String str=new String(ch);
+9        String str2=new String(ch2);
+10        if(str.equals(str2)){
+11            return true;
+12        }
+13        return false;
+14    }
+15}
+16
