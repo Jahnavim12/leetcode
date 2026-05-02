d42a0bb69e07758b85fba73456923ceac3dd2f48
Jahnavim12 <168292840+Jahnavim12@users.noreply.github.com>
2026-05-02 16:03:31 +0530

    Add solution for Kth Largest Element in an Array

---
+++ LeetCode/Kth-Largest-Element-in-an-Array/Solution.java
@@ -0,0 +1,13 @@
+1class Solution {
+2    public int findKthLargest(int[] nums, int k) {
+3        PriorityQueue<Integer> pq=new PriorityQueue<>();
+4        for(int i:nums){
+5            pq.add(i);
+6        }
+7        while(pq.size()>k){
+8            pq.poll();
+9
+10        }
+11        return pq.peek();
+12    }
+13}
