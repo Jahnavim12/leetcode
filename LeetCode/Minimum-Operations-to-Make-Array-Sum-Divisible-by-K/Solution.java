77f7659fee1e4a8962dea56884aaa0143d7bc9cd
Jahnavim12 <168292840+Jahnavim12@users.noreply.github.com>
2026-05-18 18:31:57 +0530

    Add solution for Minimum Operations to Make Array Sum Divisible by K

---
+++ LeetCode/Minimum-Operations-to-Make-Array-Sum-Divisible-by-K/Solution.java
@@ -0,0 +1,9 @@
+1class Solution {
+2    public int minOperations(int[] nums, int k) {
+3       int sum=0;
+4       for(int n:nums){
+5        sum+=n;
+6       } 
+7       return sum % k;
+8    }
+9}
