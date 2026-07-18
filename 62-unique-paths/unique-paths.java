83f58c2568a0e33535a6798779bde48352ebe835
Jahnavi Marrapu <168292840+Jahnavim12@users.noreply.github.com>
2026-07-18 16:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (98.11%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] mat=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                mat[0][j]=1;
+                mat[i][0]=1;
+                }
+                else{
+                mat[i][j]=mat[i-1][j]+mat[i][j-1];
+                }
+            }
+        }
+        return mat[m-1][n-1];
+    }
+}
