960173efd5315782bd1959a7aee89eb5a8e9c430
Jahnavi Marrapu <168292840+Jahnavim12@users.noreply.github.com>
2026-07-18 11:01:29 +0530

    Add solution for Find if Path Exists in Graph

--- LeetCode/Find-if-Path-Exists-in-Graph/Solution.java
+++ LeetCode/Find-if-Path-Exists-in-Graph/Solution.java
@@ -1,29 +1,35 @@
 1class Solution {
 2    public boolean validPath(int n, int[][] edges, int source, int destination) {
 3        Map<Integer,List<Integer>> g=new HashMap<>();
-4        for(int u=0;u<n;u++){
-5        g.put(u,new ArrayList<>());
-6    }
-7    for(int i=0;i<edges.length;i++){
-8        int u=edges[i][0],v=edges[i][1];
-9        g.get(u).add(v);
-10        g.get(v).add(u);
-11    }
-12    //    if(source==destination) return true;
-13        Queue<Integer> queues=new LinkedList<>();
-14        Set<Integer> vis=new HashSet<>();
-15        queues.add(source);vis.add(source);
-16        while(!queues.isEmpty()){
-17           int u=queues.remove();
-18           if(u==destination) return true;
-19            
-20           for(int v:g.get(u)){
-21            if(!vis.contains(v)){
-22             queues.add(v);vis.add(v);
-23
-24            }
-25           }
-26        }
-27        return false;
-28    }
-29}
+4
+5        for(int u=0;u<n;u++){
+6        g.put(u,new ArrayList<>());
+7      }
+8
+9    for(int i=0;i<edges.length;i++){
+10        int u=edges[i][0],v=edges[i][1];
+11        g.get(u).add(v);
+12        g.get(v).add(u);
+13    }
+14
+15        Queue<Integer> queues=new LinkedList<>();
+16        Set<Integer> vis=new HashSet<>();
+17        queues.add(source);vis.add(source);
+18
+19        while(!queues.isEmpty()){
+20
+21           int u=queues.remove();
+22
+23           if(u==destination) return true;
+24
+25           for(int v:g.get(u)){
+26            if(!vis.contains(v)){
+27             queues.add(v);vis.add(v);
+28            }
+29
+30           }
+31           
+32        }
+33        return false;
+34    }
+35}
