4c77691e0795a52172f312c9831d07ef608b2262
Jahnavi Marrapu <168292840+Jahnavim12@users.noreply.github.com>
2026-08-07 12:35:34 +0530

    Add solution for Longest Increasing Subsequence

---
+++ LeetCode/Longest-Increasing-Subsequence/Solution.java
@@ -0,0 +1,34 @@
+1class Solution {
+2    //static int[][] dp=new int[1001][1001];
+3    public int lengthOfLIS(int[] nums) {
+4   //  int[][] dp=new int[1001][1001];
+5        Set<Integer> st=new TreeSet<>();
+6        for(int x:nums){
+7            st.add(x);
+8        }
+9        int[] nums2=new int[st.size()];
+10        int i=0;
+11        for(int x:st){
+12            nums2[i]=x;
+13            i++; //System.out.print(x+" ");
+14        }
+15
+16        return lcslen(nums,nums2);
+17    }
+18    int lcslen(int[] a,int[] b){
+19        int n1=a.length;
+20        int n2=b.length;
+21        int[][] dp=new int[n1+1][n2+1];
+22         for(int i=n1-1;i>=0;i--){
+23            for(int j=n2-1;j>=0;j--){
+24                if(a[i]==b[j]){
+25                     dp[i][j]=1+dp[i+1][j+1];
+26                }
+27                else{
+28                     dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+29                }
+30            }
+31        }
+32        return dp[0][0];
+33    }
+34}
