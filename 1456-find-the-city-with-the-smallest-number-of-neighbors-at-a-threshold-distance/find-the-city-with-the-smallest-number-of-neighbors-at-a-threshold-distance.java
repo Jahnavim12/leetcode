296da5fa871a6cf71e01cbbb8a848269c11c2be8
Jahnavi Marrapu <168292840+Jahnavim12@users.noreply.github.com>
2026-09-18 11:18:48 +0530

    Time: 10 ms (66.85%) | Memory: 46.6 MB (39.80%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -23,12 +23,12 @@ class Solution {
 
             }
         }
-        int minCount = n;
+        int minCount = inf;
         int ans=-1;
         for (int i = 0; i < n; i++) {
         int count = 0;
         for (int j = 0; j < n; j++) {
-            if (i != j && dist[i][j] <= distanceThreshold) {
+            if ( dist[i][j] <= distanceThreshold) {
                 count++;
                 }
          }
